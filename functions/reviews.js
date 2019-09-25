2c6e62fb2c0c9ebbf58abf5288987e7fd799fbbd
Eric Chavez <eric@emcaudio.com>
2019-09-25 11:42:33 -0500

    debugging 7

--- functions/reviews.js
+++ functions/reviews.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context) {
     if (event.httpMethod !== "POST") {
-        callback(null, {
+        return {
             statusCode: 200,
             headers: {
                 "content-type": "application/json",
@@ -10,10 +10,32 @@ exports.handler = function(event, context, callback) {
                 "Access-Control-Allow-Methods": "GET, POST, PUT"
                 },
             body: "This was not a POST request!"
-            });
-    }else{
-    
-    callback(null, {
+            };
+    }
+    const parsedBody = JSON.parse(event.body)
+    const url = "mongodb+srv://eric:pUNTOFHI6IGZ3EIZ@cluster0-4coym.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    });
+
+    const Schema = mongoose.Schema;
+
+    const ReviewSchema = new Schema({
+        name: String,
+        jobTitle: String,
+        reviewBody: String
+    });
+    const Review = mongoose.model('review', ReviewSchema);
+
+
+    const review = new Review({
+        name: parsedBody.name,
+        jobTitle: parsedBody.jobTitle,
+        reviewBody: parsedBody.reviewBody,
+    })
+    review.save()
+    return {
         statusCode: 200,
         headers: {
         "content-type": "application/json",
@@ -21,6 +43,6 @@ exports.handler = function(event, context, callback) {
         "Access-Control-Allow-Methods": "GET, POST, PUT"
         },
         body: "Thank you for your review"
-        });
     }
 }
+
