755ae2b7c11c54a76b2fd706988f215730404961
Eric Chavez <eric@emcaudio.com>
2019-09-25 11:20:21 -0500

    debugging 4

--- functions/reviews.js
+++ functions/reviews.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+
 
 exports.handler = function(event, context, callback) {
     console.log('this is the event.httpMethod')
@@ -14,36 +14,4 @@ exports.handler = function(event, context, callback) {
             body: "This was not a POST request!"
             });
     }
-    const parsedBody = event.body
-    const url = "mongodb+srv://eric:pUNTOFHI6IGZ3EIZ@cluster0-4coym.gcp.mongodb.net/test?retryWrites=true&w=majority"
-    mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    });
-
-    const Schema = mongoose.Schema;
-
-    const ReviewSchema = new Schema({
-        name: String,
-        jobTitle: String,
-        reviewBody: String
-    });
-    const Review = mongoose.model('review', ReviewSchema);
-
-
-    const review = new Review({
-        name: parsedBody.name,
-        jobTitle: parsedBody.jobTitle,
-        reviewBody: parsedBody.reviewBody,
-    })
-    review.save()
-    callback(null, {
-        statusCode: 200,
-        headers: {
-        "content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT"
-        },
-        body: "Thank you for your review"
-        });
 }
